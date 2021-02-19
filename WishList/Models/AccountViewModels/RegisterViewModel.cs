af9c8d232f1f0792165b412dd818b2ae58095a3f
Ravi <ravikumar.tamanampudi@realogy.com>
2021-02-19 15:50:50 -0500

    c

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -8,6 +8,7 @@ namespace WishList.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
         [Required]
